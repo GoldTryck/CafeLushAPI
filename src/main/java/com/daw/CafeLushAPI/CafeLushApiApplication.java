200417f29a26aa19d1dee7b7a82d893bf66e8e52
Hector Larios <lariosh69@gmail.com>
2025-05-11 11:57:34 -0600

    ^[setup] configuración inicial de API

---
+++ src/main/java/com/daw/CafeLushAPI/CafeLushApiApplication.java
@@ -0,0 +1,13 @@
+package com.daw.CafeLushAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeLushApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeLushApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daw/CafeLushAPI/CafeLushApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daw.CafeLushAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeLushApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
